744a1aab7a29914825879911a5ad713ae7db6720
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2025-12-01 15:03:27 +0500

    update

--- train_resam.py
+++ train_resam.py
@@ -413,7 +413,7 @@ def train_sam(
 
                 
                 # mean_thresh = pred_stack[pred_stack > 0.5].mean()
-                mean_thresh = 0.5 + 0.2*(1-entropy_maps[0])
+                mean_thresh = 0.5 - 0.3*(entropy_maps[0])
                 pred_binary = (((pred_stack)>mean_thresh) ).float()
                 overlap_count = pred_binary.sum(dim=0)
                 overlap_map = (overlap_count > 1).float()
