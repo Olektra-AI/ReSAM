72bb2266a63bc7569fb3102cd18855a5899068fb
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2025-12-02 05:10:31 +0500

    update

--- train_resam.py
+++ train_resam.py
@@ -531,7 +531,7 @@ def train_sam(
                 loss_sim  = loss_sim
              
 
-                loss_total =  (20 * loss_focal +  loss_dice  + loss_iou +0*loss_sim     )#+ 
+                loss_total =  (20 * loss_focal +  loss_dice  + loss_iou +0.1*loss_sim     )#+ 
                 if watcher.is_outlier(loss_total):
                     continue
                 fabric.backward(loss_total)
